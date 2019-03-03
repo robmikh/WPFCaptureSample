ee534211252e3888ccfb18325eb89eec50686060
robmikh <rob.mikh@outlook.com>
2019-03-03 01:37:00 -0800

    add hmon capture

---
+++ WPFCaptureSample/MonitorEnumerationHelper.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+
+namespace WPFCaptureSample
+{
+    class DisplayInfo
+    {
+        public bool IsPrimary { get; set; }
+        public Vector2 ScreenSize { get; set; }
+        public Rect MonitorArea { get; set; }
+        public Rect WorkArea { get; set; }
+        public string DeviceName { get; set; }
+        public IntPtr Hmon { get; set; }
+    }
+
+    static class MonitorEnumerationHelper
+    {
+        delegate bool EnumMonitorsDelegate(IntPtr hMonitor, IntPtr hdcMonitor, ref RECT lprcMonitor, IntPtr dwData);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int left;
+            public int top;
+            public int right;
+            public int bottom;
+        }
+
+        public class DisplayInfoCollection : List<DisplayInfo>
+        {
+        }
+
+        private const int CCHDEVICENAME = 32;
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        internal struct MonitorInfoEx
+        {
+            public int Size;
+            public RECT Monitor;
+            public RECT WorkArea;
+            public uint Flags;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = CCHDEVICENAME)]
+            public string DeviceName;
+        }
+
+        [DllImport("user32.dll")]
+        static extern bool EnumDisplayMonitors(IntPtr hdc, IntPtr lprcClip, EnumMonitorsDelegate lpfnEnum, IntPtr dwData);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern bool GetMonitorInfo(IntPtr hMonitor, ref MonitorInfoEx lpmi);
+
+        public static DisplayInfoCollection GetDisplays()
+        {
+            DisplayInfoCollection col = new DisplayInfoCollection();
+
+            EnumDisplayMonitors(IntPtr.Zero, IntPtr.Zero,
+                delegate (IntPtr hMonitor, IntPtr hdcMonitor, ref RECT lprcMonitor, IntPtr dwData)
+                {
+                    MonitorInfoEx mi = new MonitorInfoEx();
+                    mi.Size = Marshal.SizeOf(mi);
+                    bool success = GetMonitorInfo(hMonitor, ref mi);
+                    if (success)
+                    {
+                        DisplayInfo di = new DisplayInfo();
+                        di.ScreenSize = new Vector2(mi.Monitor.right - mi.Monitor.left, mi.Monitor.bottom - mi.Monitor.top);
+                        di.MonitorArea = new Rect(mi.Monitor.left, mi.Monitor.top, di.ScreenSize.X, di.ScreenSize.Y);
+                        di.WorkArea = new Rect(mi.WorkArea.left, mi.WorkArea.top, mi.WorkArea.right - mi.WorkArea.left, mi.WorkArea.bottom - mi.WorkArea.top);
+                        di.IsPrimary = mi.Flags > 0;
+                        di.Hmon = hMonitor;
+                        di.DeviceName = mi.DeviceName;
+                        col.Add(di);
+                    }
+                    return true;
+                }, IntPtr.Zero);
+            return col;
+        }
+    }
+}
